59802208169a43de1d541d8bfb04b22c0971f927
Bhupender_Singh <bhupendersingh369@gmail.com>
2018-03-29 10:52:57 +0530

    Fourth Commit

--- src/AppController/App_Modular.java
+++ src/AppController/App_Modular.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 import PageObject.PageElementVerifiy;
 import PageObject.StoreCardCategory;
+import PageObject.VerifyImage;
 import PageObject.VerifyLinks;
 
 public class App_Modular {
@@ -15,9 +16,11 @@ public class App_Modular {
 		PageElementVerifiy pageelement=new PageElementVerifiy(driver);
 		StoreCardCategory scc=new StoreCardCategory(driver);
 		VerifyLinks links=new VerifyLinks(driver);
+		VerifyImage image=new VerifyImage(driver);
 		//links.FindallLinks(driver);
 		//links.validateInvalidLinks(driver);
-		links.FindallLinks(driver);
+		//links.FindallLinks(driver);
+		image.CheckAllImage(driver);
 		
 	//	pageelement.check();
 		//scc.Category();
